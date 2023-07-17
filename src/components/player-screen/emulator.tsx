30f986ac4c1ecdf142bb1354f71ffffddc57b486
dimitrikarpov <karpov.dimon@gmail.com>
2023-07-17 12:14:15 +0500

    style emulator component

--- src/components/player-screen/emulator.tsx
+++ src/components/player-screen/emulator.tsx
@@ -21,22 +21,30 @@ export const Emulator: React.FunctionComponent<Props> = ({
   }
 
   return (
-    <Retroarch
-      canvasBoxClassName="relative aspect-[calc(800/600)] max-h-full max-w-full"
-      onStart={onEmulatorStart}
+    <div
+      style={{
+        backgroundImage:
+          "radial-gradient( circle farthest-corner at 10% 20%,  rgba(151,10,130,1) 0%, rgba(33,33,33,1) 100.2% )",
+      }}
     >
-      <Retroarch.StartScreen className="absolute inset-0 flex h-full items-center justify-center">
-        <Retroarch.StartScreen.Button className="ease focus:shadow-outline m-2 select-none rounded-md border border-indigo-500 bg-indigo-500 px-4 py-2 text-white transition duration-500 hover:bg-indigo-600 focus:outline-none" />
-      </Retroarch.StartScreen>
-
-      <Retroarch.LoaderScreen
-        className="absolute inset-0 flex h-full items-center justify-center"
-        coreUrl={coreUrl}
-        romBinary={romBinary}
-        loadOnMount
+      <Retroarch
+        containerClassName="max-h-[100dvh] max-w-[100dvw] aspect-[calc(800/600)]  mx-auto my-0"
+        canvasBoxClassName="relative aspect-[calc(800/600)] max-h-full max-w-full"
+        onStart={onEmulatorStart}
       >
-        <Retroarch.LoaderScreen.Progress />
-      </Retroarch.LoaderScreen>
-    </Retroarch>
+        <Retroarch.StartScreen className="absolute inset-0 flex h-full items-center justify-center">
+          <Retroarch.StartScreen.Button className="ease focus:shadow-outline m-2 select-none rounded-md border border-indigo-500 bg-indigo-500 px-4 py-2 text-white transition duration-500 hover:bg-indigo-600 focus:outline-none" />
+        </Retroarch.StartScreen>
+
+        <Retroarch.LoaderScreen
+          className="absolute inset-0 flex h-full items-center justify-center"
+          coreUrl={coreUrl}
+          romBinary={romBinary}
+          loadOnMount
+        >
+          <Retroarch.LoaderScreen.Progress />
+        </Retroarch.LoaderScreen>
+      </Retroarch>
+    </div>
   )
 }
